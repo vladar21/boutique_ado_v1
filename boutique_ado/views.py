470399386ba3c3467595aec147439f4a7d6e7c38
vladar21 <vladar21@gmail.com>
2024-05-06 02:25:47 +0100

    add 404 +

--- boutique_ado/views.py
+++ boutique_ado/views.py
@@ -1,4 +1,6 @@
-from .views import handler404
+from django.shortcuts import render
 
 
-handler404 = 'boutique_ado.views.handler404'
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
